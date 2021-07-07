c860df112e9ccf2e1796bc8d7db45676e5bf7d53
sushaanpatel <sushaanpatel@gmail.com>
2021-07-07 23:52:14 +0530

    Adding products page with add route and product display in the client portal. products.html dark mode is left. Navbar align and Dark Mode on index remaining

--- finds.py
+++ finds.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template, request, redirect
+import mysql.connector
+from datetime import datetime
 
 app = Flask(__name__)
+con = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "password",
+    database = "amazonfinds"
+)
 
 global err
 err = ""
@@ -15,15 +23,33 @@ def admin():
     if request.method == "POST":
         password = request.form['password']
         if password == "1234":
-            return redirect('/add')
+            return redirect('/products')
         else:
             err = "Wrong Password"
     else:
         return render_template('pass-protect.html')
 
-@app.route('/add', methods = ["POST", "GET"])
+@app.route('/products', methods = ["POST", "GET"])
 def add():
-    ""
+    if request.method == "POST":
+        title = request.form['product-name']
+        descrip = request.form['product-description']
+        price = request.form['product-price']
+        image = request.files['product-image']
+        creator = request.form['product-creator']
+        employ_name = request.form['employ-name']
+        date = datetime.now()
+        con.reconnect()
+        db = con.cursor()
+        db.execute(f"""INSERT INTO products(name, description, price, image, creator, employ_name, date, times_clicked) VALUES("{title}", "{descrip}", "{price}", "{image.filename}", "{creator}", "{employ_name}", "{date.date()}", 0)""")
+        con.commit()
+        return redirect('/add')
+    else:
+        con.reconnect()
+        db = con.cursor()
+        db.execute("SELECT * from products")
+        product_list = db.fetchall()
+        return render_template('products.html', products = product_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
