023dc802734123cadec1167ec732caa3c3cc9980
sushaanpatel <sushaanpatel@gmail.com>
2021-08-11 22:23:48 +0530

    update price route done, also update route adapted to new db schema. most of search by catagory is done, testing is left.

--- finds.py
+++ finds.py
@@ -6,7 +6,7 @@ from models import app, db2, con, Users
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash
 from scrapper import getall, updatedb
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import LoginManager, UserMixin, login_required, current_user, login_user, logout_user
 
 # def getall(asin):
@@ -38,7 +38,10 @@ x = db.fetchall()
 for i in x:
     y = updatedb(i[1])
     rate = round(float(y['rating']))
-    db.execute(f"""UPDATE products SET price = '{y['price']}', availability = '{y['availability']}', rating = '{rate}' WHERE product_id = {i[0]}""")
+    if y['price'] != '':
+        db.execute(f"""UPDATE products SET price = '{y['price']}', availability = "{y['availability']}", rating = '{rate}' WHERE product_id = {i[0]}""")
+    else:
+        db.execute(f"""UPDATE products SET availability = "{y['availability']}", rating = '{rate}' WHERE product_id = {i[0]}""")
     con.commit()
 
 global paserr
@@ -144,8 +147,8 @@ def index():
             display_list.append(i)
     return render_template('index.html', display = display_list, prod = product_list)
 
-@app.route('/<string:asin>&id=<int:id>', methods = ["POST", "GET"])
-def product_page(asin, id):
+@app.route('/product/id=<int:id>', methods = ["POST", "GET"])
+def product_page(id):
     global current_page
     current_page = '/productpage'
     con.reconnect()
@@ -164,10 +167,21 @@ def search():
         display_list = []
         searchword = request.form['search_bar'].lower()
         cata = request.form.get('filter')
+        sub_cata = request.form.get('sub_fil')
         con.reconnect()
         db = con.cursor()
         if cata == "all":
             db.execute(f"SELECT * FROM products WHERE name LIKE '%{searchword}%'")
+            query = db.fetchall()
+            fromsearch = True
+            out = format(query)
+            for i in out:
+                display_list.append(i)
+        else:
+            if searchword != '':
+                db.execute(f"""SELECT * FROM products WHERE catagory = "{sub_cata}" and name LIKE "%{searchword}%" """)
+            else:
+                db.execute(f"""SELECT * FROM products WHERE catagory = "{sub_cata}" """)
             query = db.fetchall()
             fromsearch = True
             out = format(query)
@@ -212,7 +226,6 @@ def adminsearch():
         product_list = product_list[::-1]
         return redirect('/products')
 
-# add extra info in card
 @app.route('/products', methods = ["POST", "GET"])
 @login_required
 def add():
@@ -229,18 +242,21 @@ def add():
         except:
             proderr = "Product Not Found"
             return redirect('/products')
-        title = product['name']
+        title = product['name'].replace('"', "'")
         price = product['price']
         rating = round(float(product['rating']))
         avail = product['availability']
         image = str(product['image'])
-        descrip = str(product['descrip'])
+        descrip = str(product['descrip']).replace('"', "'")
         link = request.form['amazon-link']
         catagory = request.form.get('product-cata')
         creator = request.form['product-creator']
         employ_name = request.form['employ-name']
         date = datetime.now()
-        if link != "" and creator != "" and employ_name != "" and catagory != "-- Select Product Catagory --":
+        if price == '':
+            flash('1')
+            price = "NA"
+        if asin != "" and link != "" and creator != "" and employ_name != "" and catagory != "none":
             con.reconnect()
             db = con.cursor()
             db.execute(f"""INSERT INTO products(asin, name, description, catagory, link, image, creator, employ_name, date, times_clicked, price, rating, availability) VALUES("{asin}", "{title}", "{descrip}", "{catagory}", "{link}", "{image}", "{creator}", "{employ_name}", "{date.date()}", 0, "{price}", "{rating}", "{avail}")""")
@@ -268,6 +284,19 @@ def delete(prod_id):
     con.commit()
     return redirect('/products')
 
+@app.route('/updateprice/<int:id>', methods = ["POST", "GET"])
+def updateprice(id):
+    db = con.cursor()
+    if request.method == "POST":
+        price = "₹" + request.form['price']
+        db.execute(f"UPDATE products SET price = '{price}' WHERE product_id = {id}")
+        con.commit()
+        return redirect('/products')
+    else:
+        db.execute(f"SELECT * FROM products WHERE product_id = {id}")
+        p = db.fetchall()[0]
+        return render_template("addprice.html", p = p)
+
 @app.route('/update/<int:prod_id>', methods = ["POST", "GET"])
 def update(prod_id):
     global uperr
@@ -278,18 +307,24 @@ def update(prod_id):
         except:
             uperr = "Product Not Found"
             return redirect(f'/update/{prod_id}')
-        title = product['name']
-        image = product['image']
-        descrip = request.form['product-description']
+        title = product['name'].replace('"', "'")
+        price = product['price']
+        rating = round(float(product['rating']))
+        avail = product['availability']
+        image = str(product['image'])
+        descrip = str(product['descrip']).replace('"', "'")
         link = request.form['amazon-link']
         catagory = request.form.get('product-cata')
         creator = request.form['product-creator']
         employ_name = request.form['employ-name']
         date = datetime.now()
-        if asin != "" and descrip != "" and link != "" and creator != "" and employ_name != "" and catagory != "-- Select Product Catagory --":
+        if asin != "" and link != "" and creator != "" and employ_name != "" and catagory != "none":
             con.reconnect()
             db = con.cursor()
-            db.execute(f"""UPDATE products SET asin = "{asin}", name = "{title}", description = "{descrip}", catagory = "{catagory}", link = "{link}", image = "{image}", creator = "{creator}", employ_name = "{employ_name}", date = "{date.date()}" WHERE product_id = "{prod_id}" """)
+            if price == '':
+                db.execute(f"""UPDATE products SET asin = "{asin}", name = "{title}", description = "{descrip}", catagory = "{catagory}", link = "{link}", image = "{image}", creator = "{creator}", employ_name = "{employ_name}", date = "{date.date()}", availability = "{avail}", rating = {rating} WHERE product_id = "{prod_id}" """)
+            else:
+                db.execute(f"""UPDATE products SET asin = "{asin}", name = "{title}", description = "{descrip}", catagory = "{catagory}", link = "{link}", image = "{image}", creator = "{creator}", employ_name = "{employ_name}", date = "{date.date()}", price = "{price}", availability = "{avail}", rating = {rating} WHERE product_id = "{prod_id}" """)
             con.commit()
             return redirect('/products')
         else:
